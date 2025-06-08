6f59cc8b82db0ff4bdb640cb676bfa3632b8843c
vorona_af <vorona_af@magnit.ru>
2025-06-08 14:29:11 +0300

    Доработка по результатам код ревью

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,13 +1,12 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Service;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,22 +24,20 @@ public class UserController {
     }
 
     @PostMapping
-    public User create(@RequestBody User user) {
+    public User create(@Valid @RequestBody User user) {
         log.debug("Начало добавления пользователя {}", user);
-        if (validate(user)) {
-            user.setId(Service.getNextId(users));
-            if (user.getName() == null) {
-                user.setName(user.getLogin());
-                log.debug("Для пользователя в качестве имени указан логин");
-            }
-            users.put(user.getId(), user);
+        user.setId(Service.getNextId(users));
+        if (user.getName() == null) {
+              user.setName(user.getLogin());
+              log.debug("Для пользователя в качестве имени указан логин");
         }
+        users.put(user.getId(), user);
         log.debug("Добавлен пользователь {}", user);
         return user;
     }
 
     @PutMapping
-    public User update(@RequestBody User newUser) {
+    public User update(@Valid @RequestBody User newUser) {
         if (users.containsKey(newUser.getId())) {
             log.debug("Начало обновления пользователя {}", newUser);
             User oldUser = users.get(newUser.getId());
@@ -54,31 +51,6 @@ public class UserController {
         }
         log.error("Пользователь с id = {} не найден", newUser.getId());
         throw new NotFoundException("Пользователь с id = " + newUser.getId() + " не найден");
-    }
-
-    public static boolean validate(User user) {
-        String textExp = "";
-        if (user.getEmail().isBlank()) {
-            log.error("Электронный адрес не может быть пустым, пользователь {}", user);
-            throw new ValidationException("Электронный адрес не может быть пустым");
-        }
-        if (!user.getEmail().contains("@")) {
-            log.error("Неправильный формат электронного адреса, пользователь {}", user);
-            throw new ValidationException("Неправильный формат электронного адреса");
-        }
-        if (user.getLogin().isBlank()) {
-            log.error("Логин не может быть пустым, пользователь {}", user);
-            throw new ValidationException("Логин не может быть пустым");
-        }
-        if (user.getLogin().contains(" ")) {
-            log.error("Логин не может содержать пробелы, пользователь {}", user);
-            throw new ValidationException("Логин не может содержать пробелы");
-        }
-        if (user.getBirthday().isAfter(LocalDate.now())) {
-            log.error("Дата рождения не может быть в будущем, пользователь {}", user);
-            throw new ValidationException("Дата рождения не может быть в будущем");
-        }
-        return true;
     }
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,5 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,10 +13,17 @@ import java.time.LocalDate;
 @Data
 @Builder
 public class User {
-    Long id;
-    String email;
-    String login;
-    String name;
-    LocalDate birthday;
+    //заполняем автоматически
+    private Long id;
+    @NotBlank(message = "Адрес почты должен быть указан")
+    @Email(message = "Неправильный формат электронной почты")
+    private String email;
+    @NotBlank(message = "Логин должен быть указан")
+    private String login;
+    //имя может быть пустым, тогда берется из логина
+    private String name;
+    @NotNull(message = "День рождения должен быть указан")
+    @Past(message = "День рождения должен быть в прошлом")
+    private LocalDate birthday;
 
 }
