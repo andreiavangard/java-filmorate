eb58fa748587e425114e7948f6df58bb3e62c8f7
vorona_af <vorona_af@magnit.ru>
2025-08-31 18:35:12 +0300

    ДЗ 11 замечания автопроверки

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -33,7 +33,7 @@ public class User {
     private LocalDate birthday;
     @Builder.Default
     private Set<Long> friends = new HashSet<>();
-    
+
     public void setFriend(Long userId) {
         if (!friends.contains(userId)) {
             friends.add(userId);
