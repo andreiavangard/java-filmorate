dc7569f1c1f5bb779e216c3aa8fd512ec841016d
vorona_af <vorona_af@magnit.ru>
2025-09-09 22:35:55 +0300

    ДЗ 12

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,13 +3,11 @@ package ru.yandex.practicum.filmorate.controller;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.User.UserService;
-import ru.yandex.practicum.filmorate.storage.User.UserStorage;
 
 import java.util.Collection;
 import java.util.Optional;
