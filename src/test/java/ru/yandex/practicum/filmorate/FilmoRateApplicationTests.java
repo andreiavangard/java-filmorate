b58d99ef98d66fdd7b23417060cf6e85e749d64e
vorona_af <vorona_af@magnit.ru>
2025-09-09 23:55:07 +0300

    ДЗ 12

--- src/test/java/ru/yandex/practicum/filmorate/FilmoRateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmoRateApplicationTests.java
@@ -1,34 +1,13 @@
 package ru.yandex.practicum.filmorate;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.context.annotation.Import;
-import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.User.UserDbStorage;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Optional;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-@JdbcTest
-@AutoConfigureTestDatabase
-@RequiredArgsConstructor(onConstructor_ = @Autowired)
-@Import({UserDbStorage.class})
-public class FilmoRateApplicationTests {
-    private final UserDbStorage userStorage;
+@SpringBootTest
+class FilmorateApplicationTests {
 
     @Test
-    public void testFindUserById() {
-
-        Optional<User> userOptional = userStorage.findById((long)1);
-
-        assertThat(userOptional)
-                .isPresent()
-                .hasValueSatisfying(user ->
-                        assertThat(user).hasFieldOrPropertyWithValue("id", 1)
-                );
+    void contextLoads() {
     }
+
 }
